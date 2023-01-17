6938e5bbf2ae824b2bb5d72ca2847ddf0edc0f05
Karimli Aghabala <54455724+karimliaghabala@users.noreply.github.com>
2023-01-17 22:54:45 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+function cavab(a)
+{
+    if (a==2)
+    {
+        document.getElementById('z').innerHTML="Cavab Doğrudur.";
+        document.getElementById('z').style.color="green"
+    }
+    else
+    {
+        alert ("Təəssüf ki, doğma vətənin haqqında ən sadə məlumatları bilmirsən");
+    }
+};
+
+
+
+        function cavabb(b)
+        {
+            if (b==4)
+            {
+                document.getElementById('zengilan').innerHTML="Cavab Doğrudur.";
+                document.getElementById('zengilan').style.color="green";
+            }
+            else
+            {
+                alert ("Təəssüf ki, doğma vətənin haqqında ən sadə məlumatları bilmirsən");
+            }
+        };
+
+
+                function cavabbb(c)
+                {
+                    if (c==7)
+                    {
+                        document.getElementById('ozbek').innerHTML="Cavab Doğrudur.";
+                        document.getElementById('ozbek').style.color="green";
+                    }
+                    else
+                    {
+                        alert ("Təəssüf ki, doğma vətənin haqqında ən sadə məlumatları bilmirsən");
+                    }
+                };
